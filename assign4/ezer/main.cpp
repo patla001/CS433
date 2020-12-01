ac216a4dfb1fe665f098726c77a9ba14b11bbe48
Ezer <patla001@cougars.csusm.edu>
2020-11-30 16:07:20 -0800

    Completed Assignment 4

--- assign4/ezer/main.cpp
+++ assign4/ezer/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <vector>
 //#include "buffer.h"
 
 // required for rand() library
@@ -18,133 +19,82 @@ using namespace std;
 pthread_mutex_t mutex;
 sem_t full;
 sem_t empty;
-int in = 0;
-int out = 0;
-// pthread attribute
-//pthread_attr_t attr;
+int count = 0;
 const int BUFFER_SIZE = 5;
 int RAND_RANGE = 100 + 1;
-int bufferItem[BUFFER_SIZE];
-void display();	
+vector<int> buffer;
 
 void *producer(void *param)
 {       
-  //buffer_item item;
-  //int item;
-	//lock.acquire();
-        
-  //while (count == BUFFER_SIZE)
   while (true)
   {
-      //sleep(rand() % 3); 	   
+      // sleep for a random period of time	   	
       usleep(rand() % 1000000);
+      // generate a random number between 1 - 100
       int item = rand() % RAND_RANGE + 1;    
-      //wait(&space, &lock)
       // obtained the semaphore and wait until is not full
       sem_wait(&empty);
       // make sure the mutex access to the critical section
-      // lock.acquire()
       pthread_mutex_lock(&mutex);
-                
-      // change between sleeping and producing
-      // sleep for a random period of time
-      //usleep(rand()%1000000);
-      // generate a random number
-      //item = rand();
-        
 	
-       cout << "producer random: " << item << endl;
-
-	bufferItem[in] = item;
-	printf("Producer %d: Insert Item %d at %d\n", in,bufferItem[in],in);
-	in = ( in + 1) % BUFFER_SIZE; 
-
-	cout << "The current content of the buffer is [ ";
-	for (int i = 0; i < in; i++)
+	if (count != BUFFER_SIZE)
 	{
-		cout << bufferItem[i] << " ";
-		
+		buffer.push_back(item);
+		cout << "item " << item << " inserted by a producer." << endl;
+		count++;
+
+		cout << "The current buffer vector is [ ";
+                for (unsigned int i = 0; i <buffer.size(); i++)
+                {
+                        cout << buffer[i] << " ";
+                }
+                cout << "]" << endl;
+
 	}
-	cout << "]" << endl;
       // critical section ...
-/*       if (insert_item(item) == 0)
-	{       
-	  cout << "Error: cannot produce item" << endl;
-	  //fprintf("error: cannot produce item");
-	  } else {
-		std::cout << "item " << item << " inserted by a producer." << std::endl;
-		display();
-	} 
-*/
-/*        if (insert_item(item) == 0  ) 
-	{
-	    std::cout << "item " << item << " inserted by a producer." << std::endl;
-	     //printf("producer produced %d\n", item);
-	     display();
-	} else {
-	    cout << "Error: cannot produce item" << endl;
-	}*/
 	cout << endl;
       // unlock the mutex
       pthread_mutex_unlock(&mutex);
       // release the semaphore
       sem_post(&full);
-      // increment the counter
-      //      count++;
     }
 
         
-  //return 0;
 }
 
 void *consumer(void *param)
 {
-  //buffer_item item;
-	//int item;
   while (true)
   {
-      //sleep(rand() % 3);
+	
+      // sleep for a random period of time
       usleep(rand() % 1000000);
       // obtained the semaphore and wait until is not full                                                                                                                                  
       sem_wait(&full);
-
       // make sure the mutex access to the critical section                                                                                                                                 
       pthread_mutex_lock(&mutex);
 
-      // sleep for a random period of time                                                                                                                                                  
-      //usleep(rand()%1000000);
-
-
       // critical section ...                                                                                                                                                               	
-     	 
-      //cout << "consumer random: " << item << endl;
+	if (count != 0)
+	{
+		int back = buffer.size();
+		int item = buffer[back-1];
+		buffer.erase(buffer.begin()+back-1);
+		cout << "item " << item << " removed by a consumer." << endl;
+		count--;
 
-	int item = bufferItem[out];
-	cout << "consumer random: " << item << endl;
-	printf("Consumer %d: Remove Item %d from %d\n",out, item, out);
-	out = (out + 1) % BUFFER_SIZE;	
-	cout << "The current content of the buffer is [ ";
-        for (int i = 0; i < out; i++)
-        {
-                cout << bufferItem[i] << " ";
-
-        }
-        cout << "]" << endl;
-													     
- /*     if (remove_item(&item) == 0  ) 
-      {
-	     //printf("consumer consumed %d\n", item);
-	     std::cout << "item " << item << " removed by a consumer.  " << std::endl;
-	     display();
-      } else {
-	     cout << "Error: cannot consume an item" << endl;
-      }
-*/
+		cout << "The current buffer vector is [ ";
+		for (unsigned int i = 0; i <buffer.size(); i++)
+		{
+			cout << buffer[i] << " ";
+		}
+		cout << "]" << endl;
+	}											     
+      
       cout << endl;
       // unlock the mutex                                                                                                                                                                   
       pthread_mutex_unlock(&mutex);
       // release the semaphore                                                                                                                                                              
-      // decrease until empty                                                                                                                                                               
       sem_post(&empty);
     }
 }
@@ -184,8 +134,6 @@ int main(int argc, char *argv[])
 	pthread_t consumer_threads[cthreadc];
 	pthread_mutex_init(&mutex, NULL);
         
-	// pthread attribute variable
-	//pthread_attr_init(&attr);
 	
 	
 	// empty is initialized to the buffer size
@@ -196,7 +144,6 @@ int main(int argc, char *argv[])
 	// 3. Create producer thread(s)
 	for (int i = 0; i < pthreadc; i++)
 	{
-		//pthread_create(&producer_threads[i], NULL, producer, &bufferItem[i]);
 		pthread_create(&producer_threads[i], NULL, producer, NULL);
 	}
 
@@ -204,12 +151,11 @@ int main(int argc, char *argv[])
 	
 	for (int i=0; i < pthreadc; i++)
 	{
-		//pthread_create(&consumer_threads[i], NULL, consumer, &bufferItem[i]);
 		pthread_create(&consumer_threads[i], NULL, consumer, NULL);
 	}
 
 	// 5. Sleep
-	sleep(sleeptime);
+	usleep(sleeptime*1000000);
 	//usleep(sleeptime*300000);
 	//usleep(sleeptime % 5000000);
 	
